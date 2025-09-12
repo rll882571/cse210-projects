15d9f59a50063856315ac2b412d23806a3818b43
rfl882571 <rfl882571@gmail.com>
2025-09-12 09:41:37 -0300

    h

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,15 +7,8 @@ class Program
 
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-
-        Console.Write("Whait is your first name? ");
-        string first = Console.ReadLine();
-
         Console.Write("Whait is your last name? ");
         string last = Console.ReadLine();
-
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
