6e697b3c2c5495034ea19ce1ffde7c1a6c017975
rll882571 <del22007@byui.edu>
2025-09-12 23:49:48 -0300

    5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        
+        // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int numberOfTries = 0;
+    
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numberOfTries++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {numberOfTries} tries.");
+            }
+
+        }                    
     }
 }
