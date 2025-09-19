99448c09a8c06cb435ec926c92227c32b65980eb
rfl882571 <rfl882571@gmail.com>
2025-09-19 12:00:05 -0300

    hi

--- week02/Journal/jornal.cs
+++ week02/Journal/jornal.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 public class Journal
 {
@@ -26,7 +27,7 @@ public class Journal
         {
             foreach (Entry entry in _entries)
             {
-                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+                outputFile.WriteLine($"\"{entry._date}\",\"{entry._promptText}\",\"{entry._entryText}\",\"{string.Join(";", entry._tags)}\"");
             }
         }
     }
@@ -38,12 +39,17 @@ public class Journal
 
         foreach (string line in lines)
         {
-            string[] parts = line.Split('|');
-
+            string[] parts = line.Split(new string[] { "\",\"" }, StringSplitOptions.None);
+            
             Entry newEntry = new Entry();
-            newEntry._date = parts[0];
+            newEntry._date = parts[0].Trim('"');
             newEntry._promptText = parts[1];
-            newEntry._entryText = parts[2];
+            newEntry._entryText = parts[2].Trim('"');
+            
+            if (parts.Length > 3)
+            {
+                newEntry._tags = new List<string>(parts[3].Trim('"').Split(';'));
+            }
 
             _entries.Add(newEntry);
         }
