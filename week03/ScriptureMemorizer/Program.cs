3f014a5332475b4d00f1c529609e00f96a9cc5e8
rfl882571 <rfl882571@gmail.com>
2025-09-26 12:14:00 -0300

    k

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,24 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
-
 public class Program
 {
     public static void Main(string[] args)
     {
         
-
-        Reference reference = new Reference("John", 3, 16); 
-
         
-
-
-       
         List<Scripture> availableScriptures = new List<Scripture>();
 
         
-
         Reference ref1 = new Reference("John", 3, 16); 
         string text1 = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
         availableScriptures.Add(new Scripture(ref1, text1));
@@ -38,7 +29,7 @@ public class Program
             Console.WriteLine("--- Scripture Memorizer Game ---");
             Console.WriteLine("Please select a scripture to memorize:");
 
-            
+           
             for (int i = 0; i < availableScriptures.Count; i++)
             {
                 
@@ -53,7 +44,7 @@ public class Program
             if (menuInput == "quit")
             {
                 keepRunning = false;
-                break; 
+                break; // Sai do loop principal
             }
 
             
@@ -75,7 +66,7 @@ public class Program
                 string postGameInput = Console.ReadLine();
                 if (postGameInput != "1")
                 {
-                    keepRunning = false; // Se não for 1, encerra
+                    keepRunning = false;
                 }
             }
             else
@@ -93,41 +84,28 @@ public class Program
     {
         string userInput = "";
         
-       
-        while (userInput.ToLower() != "quit" && !scripture.IsCompletelyHidden())
-        {
-            
-        
-        Scripture scripture = new Scripture(reference, scriptureText);
-
-        
-        string userInput = "";
-
         
         while (userInput.ToLower() != "quit" && !scripture.IsCompletelyHidden())
         {
             
-
             Console.Clear();
 
             
             Console.WriteLine(scripture.GetDisplayText());
 
-            // Solicita a entrada do usuário
+           
             Console.WriteLine("\nPress Enter to hide more words or type 'quit' to end the current scripture:");
             userInput = Console.ReadLine();
 
             
             if (userInput.ToLower() != "quit")
             {
-                
+               
                 scripture.HideRandomWords(3);
             }
         }
 
-
-
-        
+       
         if (scripture.IsCompletelyHidden())
         {
             Console.Clear();
@@ -136,9 +114,8 @@ public class Program
         }
         else
         {
-            // Caso o usuário tenha digitado 'quit'
+            
             Console.WriteLine("Program ended by user.");
         }
-
     }
 }
